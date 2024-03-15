a95ba069117c2a32b0c32b3ff29436dca38a0c81
aqlt <alain.quartier@yahoo.fr>
2024-03-15 12:11:03 +0000

    rename functions

--- R_simul_quarter/1A_data_creation.R
+++ R_simul_quarter/1A_data_creation.R
@@ -10,10 +10,10 @@ set.seed(100)
 start = 1960
 frequency = 4
 time = seq_along(seq(start, 2019+3/4, by = 1/4))
-series = list(
-  highvariability1 = simulated_tci(time,sigma_nu = 0.08,sigma_e = 1.80,lambda = 72/4,rho = 0.2),
-  highvariability2 = simulated_tci(time,sigma_nu = 0.08,sigma_e = 1.80,lambda = 72/4,rho = 0.5),
-  highvariability3 = simulated_tci(time,sigma_nu = 0.08,sigma_e = 1.80,lambda = 72/4,rho = 0.6),
+series_q = list(
+  highvariability1 = simulated_tci(time,sigma_nu = 0.08,sigma_e = 0.60,lambda = 72/4,rho = 0.5),
+  highvariability2 = simulated_tci(time,sigma_nu = 0.08,sigma_e = 0.60,lambda = 72/4,rho = 0.7),
+  highvariability3 = simulated_tci(time,sigma_nu = 0.08,sigma_e = 0.60,lambda = 72/4,rho = 1.0),
   mediumvariability1 = simulated_tci(time,sigma_nu = 0.08,sigma_e = 0.50,lambda = 72/4,rho = 1.5),
   mediumvariability2 = simulated_tci(time,sigma_nu = 0.08,sigma_e = 0.50,lambda = 72/4,rho = 2),
   mediumvariability3 = simulated_tci(time,sigma_nu = 0.08,sigma_e = 0.50,lambda = 72/4,rho = 3),
@@ -21,24 +21,32 @@ series = list(
   lowvariability2 = simulated_tci(time,sigma_nu = 0.08,sigma_e = 0.40,lambda = 72/4,rho = 3.5),
   lowvariability3 = simulated_tci(time,sigma_nu = 0.08,sigma_e = 0.40,lambda = 72/4,rho = 4)
 )
-series = lapply(series,ts, start = start, frequency = frequency)
-do.call(rbind, lapply(series, function(x){
-  x <- x[, "tc"]
+series_q = lapply(series_q,ts, start = start, frequency = frequency)
+do.call(rbind, lapply(series_q, function(x){
+  x <- x[,"tc"]
   res = rjd3x11plus::select_trend_filter(x, length = 5)
   res = c(res[c("length", "icr")],
           rjd3x11plus::select_trend_filter(x, length = 7)[1])
   names(res) = c("optimal_length", "icr-5", "icr-7")
   res
 }))
+forecast::autoplot(do.call(cbind, lapply(series_q[c(2,5,8)],`[`, ,"tc")))  +
+  theme_bw() +theme(legend.background = element_rect(fill = alpha('gray99', 0.4),
+                                                     colour = "gray80", linetype = "solid"),
+                    legend.justification = c(0,0),
+                    legend.position = c(0,0),
+                    legend.key = element_blank(),
+                    legend.title = element_blank()) +
+  labs(x = NULL, y = NULL)
 
-tp = turning_points(series[[1]][,"cycle"])
-first_date = time(series[[9]][,"cycle"])[25]
+tp = turning_points(series_q[[1]][,"cycle"])
+first_date = time(series_q[[9]][,"cycle"])[25]
 tp = lapply(tp, function(x)x[x>=first_date])
 saveRDS(tp, "data_simul_trim/tp_simul1.RDS")
 
-for(nom_series in names(series)){
+for(nom_series in names(series_q)){
   all_series <- lapply(time[-(1:24)], function(i){
-    ts(series[[nom_series]][1:i,"tc"], start = start, frequency = frequency)
+    ts(series_q[[nom_series]][1:i,"tc"], start = start, frequency = frequency)
   })
   names(all_series) <- sapply(all_series, function(x) time(x)[length(x)])
   saveRDS(all_series, file = sprintf("data_simul_trim/byseries/%s.RDS", nom_series))
@@ -47,7 +55,6 @@ for(nom_series in names(series)){
 
 library(future)
 plan(multisession)
-
 
 fs <- list()
 i <- 0
@@ -62,6 +69,8 @@ for(s in list.files("data_simul_trim/byseries",full.names = TRUE)){
       res = c(res[c("length", "icr")],
               rjd3x11plus::select_trend_filter(x, length = 7)[1])
       names(res) = c("optimal_length", "icr-5", "icr-7")
+      # ic-ratio where multiplied by 3 for quarterly series
+      res[-1] <- res[-1] / 3
       res
     })
     saveRDS(info, sprintf("data_simul_trim/byseriesinfo/%s", basename(s)))
